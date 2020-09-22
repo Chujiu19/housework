9d398b81363250d5bf5fac684c6734819c11e848
chujiu19 <w19191999@163.com>
2020-09-23 02:53:48 +0800

    compile

--- 03-01/code/vue/src/praser.js
+++ 03-01/code/vue/src/praser.js
@@ -1,21 +1,35 @@
+
+import { h } from 'snabbdom'
+
 function isText(node) {
     return node && node.nodeType == 3
 }
 function isElement(node) {
     return node && node.nodeType == 1
 }
-export praser(node) {
+
+export function praserHtml(node) {
     if (isElement(node)) {
-        const { tagName, nodeType, attributes, children } = node
-        const data = Array.from(attributes).reduce((attrMap, attr) => {
-            attrMap[attr.name] = attr.value
-            return attrMap
-        }, {})
-        const children = Array.from(children).map(child => praser(child))
+        const { tagName, attributes, children } = node
+        const attrs = {}, directs = {}
+        Array.from(attributes).forEach(({ name, value }) => {
+            let reg = /^v-(.+)(?:.+)/
+            if (reg.test(name)) {
+                const [direct, attr] = name.match(reg)
+                directs[direct] = directs[direct] || []
+                directs[direct].push({
+                    attr: arg[1],
+                    value: `b_(c => ${value})`
+                })
+            } else {
+                attrs[name] = value
+            }
+        })
         return {
             tag: String(tagName).toLowerCase(),
-            data,
-            children
+            attrs,
+            directs,
+            children: Array.from(children).map(child => praser(child))
         }
     } else if (isText(node)) {
         let content = node.testContent.trim()
@@ -36,7 +50,52 @@ export praser(node) {
     }
 }
 
-export creater(tag, data, children) {
+export function creater(tag, data, children) {
+    const newNode = {
+        tag
+    }
+
+    if (data.bind) {
+        const bind = data.bind
+        newNode.directs = Object.keys(bind).reduce((directs, attr) => {
+            directs[attr] = bind[attr]
+            return directs
+        }, {})
+    }
+    if (data.on) {
+        const on = data.on
+        newNode.directs = newNode.directs || {}
+
+        Object.keys(on).forEach(attr => {
+            newNode.directs.on[attr] = on[attr]
+        }, {})
+    }
+
+    let nChildren
+
+    if(tag) {
+
+    }
+
+    let attrs = {}, directs = {}
+    Object.keys(data).forEach(key => {
+        directs[key] = directs[key] || []
+        directs[key].push({
+            attr: 
+        })
+        if (key == 'on') {
+            Object.keys(data[key]).forEach()
+        }
+    })
+
+
+
+    if (tag) {
+
+    } else {
+
+    }
+
     return {
         tag,
         data,
