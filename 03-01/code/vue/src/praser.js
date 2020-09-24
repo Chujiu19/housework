c319f11087f4692ef591f2ca786774b29e476e89
chujiu19 <w19191999@163.com>
2020-09-24 21:37:17 +0800

    111

--- 03-01/code/vue/src/praser.js
+++ 03-01/code/vue/src/praser.js
@@ -5,9 +5,27 @@ function isElement(node) {
     return node && node.nodeType == 1
 }
 export praser(node) {
+    let render = `function(){return h(`
     if (isElement(node)) {
+        render += tagName + ','
+        let props = {
+            attrs: {},
+            props: {}
+        }
         const { tagName, nodeType, attributes, children } = node
-        const data = Array.from(attributes).reduce((attrMap, attr) => {
+        Array.from(attributes).forEach((attr) => {
+            const { name, value } = attr
+            const reg = /^v-(\w+)(:\w+)?((?:\.\w+)*)$/
+            if(name && reg.test(name)) {
+                const [,d, e, a] = name.match(reg)
+                if(d) {
+                    if(d == 'bind') props.props[e] = a
+                    if(d == 'on') props.props.on ? props.props.on[e] = (a())() : props.props.on = {e: a}
+                }
+            }else {
+                props.attrs[name] = value
+            }
+            if(attr.name.startWith('v-'))
             attrMap[attr.name] = attr.value
             return attrMap
         }, {})
@@ -21,6 +39,7 @@ export praser(node) {
         let content = node.testContent.trim()
         let data = {}
         if (content) {
+
             content = content.replace(/(\{\{).*?(\}\})/g, (attr) => {
                 const val = attr.slice(2, attr.length - 2)
                 const key = `R_(${val})`
